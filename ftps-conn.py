af42977841a6528789257e67f577965d7d6a7731
pyphil <63059088+pyphil@users.noreply.github.com>
2021-03-20 23:16:07 +0100

    Create ftps-conn.py

---
+++ ftps-conn.py
@@ -0,0 +1,57 @@
+from ftplib import FTP_TLS
+import ssl
+
+class FTPS_conn:
+    def __init__(self, url, krzl, pw):
+        self.url = url
+        self.krzl = krzl
+        self.pw = pw
+
+    def connect(self):
+        ftp = FTP_TLS()
+        # using TLS version 1.2
+        # ftp.ssl_version = ssl.PROTOCOL_TLSv1_2
+        # ftp.debugging = 2
+        try:
+            ftp.connect(self.url, 21)
+        except:
+            return "hosterr"
+        else:
+            try:
+                ftp.login(self.krzl, self.pw)
+            except:
+                return "loginerr"
+            else:
+                ftp.prot_p()
+                return ftp
+
+    def download_kursdb(self):
+        ftp = self.connect()
+        if ftp == "hosterr":
+            print("Servername falsch")
+        elif ftp == "loginerr":
+            print("Login falsch")
+        else:
+            # Datei herunterladen
+            with open ('kurs.db','wb') as localfile:
+                ftp.retrbinary('RETR kurs.db', localfile.write)
+            ftp.close()
+
+    def upload_kursdb(self):
+        ftp = self.connect()
+        if ftp == "hosterr":
+            print("Servername falsch")
+        elif ftp == "loginerr":
+            print("Login falsch")
+        else:    
+            # Datei hochladen rb -> read binary
+            with open ('zwei.txt','rb') as fp:
+                ftp.storbinary('STOR zwei.txt', fp)
+            ftp.close()
+
+    def upload_timestamp(self):
+        pass
+
+if __name__ == "__main__":
+    conn = FTPS_conn("url","krz","pw")
+    conn.download_kursdb()
