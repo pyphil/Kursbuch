ebb8da8d82c68f0cdc8e454793bcaed3d40ad5a3
pyphil <63059088+pyphil@users.noreply.github.com>
2021-08-06 23:13:54 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,6 +14,13 @@ python3 -OO -m PyInstaller \
     --add-data add-members.png:. \
     --add-data delete-member.png:. \
     --add-data delete-members.png:. \
+    --add-data delcourse.png:. \
+    --add-data delstunde.png:. \
+    --add-data kursmitglieder.png:. \
+    --add-data neuestunde.png:. \
+    --add-data new_course.png:. \
+    --add-data pdf.png:. \
+    --add-data tutor.png:. \
     --add-data kursbuch.ico:. \
     --add-data LICENSE:. \
     --add-data ferien.db:. \
