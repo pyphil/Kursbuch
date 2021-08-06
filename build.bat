38399dc7baf62956ba8470ba1f7a274d2e60ab3a
pyphil <63059088+pyphil@users.noreply.github.com>
2021-08-06 23:05:04 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -15,6 +15,13 @@ python.exe -OO -m PyInstaller ^
     --add-data add-members.png;. ^
     --add-data delete-member.png;. ^
     --add-data delete-members.png;. ^
+    --add-data delcourse.png;. ^
+    --add-data delstunde.png;. ^
+    --add-data kursmitglieder.png;. ^
+    --add-data neuestunde.png;. ^
+    --add-data new_course.png;. ^
+    --add-data pdf.png;. ^
+    --add-data tutor.png;. ^
     --add-data kursbuch.ico;. ^
     --add-data LICENSE;. ^
     --add-data ferien.db;. ^
